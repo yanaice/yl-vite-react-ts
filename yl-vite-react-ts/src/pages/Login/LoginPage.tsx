032cc4360d1aaee2f1eb31edc5323c5e02014b75
yanaice <yana.l@kbtg.tech>
2021-10-09 18:03:08 +0700

    loginpage

--- yl-vite-react-ts/src/pages/Login/LoginPage.tsx
+++ yl-vite-react-ts/src/pages/Login/LoginPage.tsx
@@ -30,54 +30,56 @@ const LoginPage = ({ message }: Props): JSX.Element => {
 
   return (
     <PageLayout
-      header={<div className="nav-bar">MENU =</div>}
+      header={<div className="nav-bar">Welcome to ...</div>}
       body={
-        <div className="p-4 bg-blue-200">
-          <div>1st</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LOGIN</div>
-          <div>LSSDT</div>
+        <div className="h-full flex justify-center items-center bg-gray-100">
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col">
+            <div className="mb-4">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-red text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+                type="button"
+              >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </div>
         </div>
       }
       footer={
